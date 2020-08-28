2743af0bee8779913c1bf8abe2ff03145d05d26d
Anthony Almeida <anthony.almeida@chipinside.com.br>
2020-08-28 15:14:57 -0300

    Add Input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,19 +1,34 @@
+import { forwardRef } from 'react'
 import * as S from './styles'
 
-interface InputProps {
-  label?: string
-  placeholder?: string | ''
+type Error = {
+  message?: string | undefined
 }
 
-const Input: React.FC<
-  InputProps & React.InputHTMLAttributes<HTMLInputElement>
-> = ({ label, placeholder, ...rest }) => {
-  return (
-    <>
-      {label ? <S.Label>{label}</S.Label> : null}
-      <S.Input placeholder={placeholder} {...rest} />
-    </>
-  )
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  label?: string
+  placeholder?: string | ''
+  error?: Error
+  hasError?: boolean
 }
+
+// eslint-disable-next-line react/display-name
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ label, placeholder, error, ...rest }, ref) => {
+    return (
+      <>
+        {label ? <S.Label>{label}</S.Label> : null}
+        <S.Input
+          ref={ref}
+          placeholder={placeholder}
+          hasError={!!error}
+          {...rest}
+        />
+        {!!error?.message && <S.Error>{error.message}</S.Error>}
+      </>
+    )
+  }
+)
 
 export default Input
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,9 +1,11 @@
 import styled from 'styled-components'
+import { InputProps } from './index'
 
-export const Input = styled.input`
+export const Input = styled.input<InputProps>`
   background-color: #6a7b85;
-  color: white;
+  color: ${(props) => (props.hasError ? '#ed1923' : 'white')};
   border: none;
+  border-bottom: ${(props) => (props.hasError ? '2px solid #ed1923' : 'none')};
   border-radius: 25px;
   margin-bottom: 10px;
   padding: 10px;
@@ -13,12 +15,21 @@ export const Input = styled.input`
     background-color: #6a6a6a;
   }
 
+  :focus {
+    outline-width: 0;
+  }
+
+  :disabled {
+    background-color: #6a6a6a;
+    cursor: not-allowed;
+  }
+
   ::placeholder,
   ::-webkit-input-placeholder {
-    color: #fff;
+    color: ${(props) => (props.disabled ? '#8a949a' : 'white')};
   }
   :-ms-input-placeholder {
-    color: #fff;
+    color: ${(props) => (props.disabled ? '#8a949a' : 'white')};
   }
 
   -webkit-box-shadow: 8px 6px 11px -1px rgba(0, 0, 0, 0.75);
@@ -30,4 +41,13 @@ export const Label = styled.span`
   text-align: 'left';
   padding: 2px;
   font-size: 1.5rem;
+`
+
+export const Error = styled.span`
+  text-align: 'left';
+  padding: 2px;
+  font-size: 1.5rem;
+  color: #ed1923;
+  margin-bottom: 8px;
+  text-shadow: 0px 1px 3px rgba(0, 0, 0, 0.75);
 `
