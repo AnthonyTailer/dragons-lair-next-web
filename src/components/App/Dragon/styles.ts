7569919f4174674e4ecbc8fa4f103c57fd954428
Anthony Almeida <anthony.almeida@chipinside.com.br>
2020-08-28 22:26:20 -0300

    Add Dragon's List Page

---
+++ src/components/App/Dragon/styles.ts
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  width: 100%;
+  height: 100%;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+  text-align: center;
+  margin-bottom: 0.8em;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+  margin-bottom: 1em;
+  text-align: center;
+`
